afca5814c090a13c748b979f9470f293ce5bb524
Mike-RyzenPC\Mike <Tatlreach@gmail.com>
2019-09-07 13:59:45 -0500

    implemented the authors version of schlick. made a hollow sphere inside the glass sphere.

--- Ray_Tracer/Ray_Tracer/src/dielectric.h
+++ Ray_Tracer/Ray_Tracer/src/dielectric.h
@@ -1,6 +1,12 @@
 #pragma once
 #include "material.h"
 
+float schlick(float cosine, float ref_idx) {
+	float r0 = (1 - ref_idx) / (1 + ref_idx);
+	r0 = r0 * r0;
+	return r0 + (1 - r0)*pow((1 - cosine), 5);
+}
+
 bool refract_author(const vec3& v, const vec3& n, float ni_over_nt, vec3& refracted) {
 	vec3 uv = unit_vector(v);
 	float dt = dot(uv, n);
@@ -20,26 +26,39 @@ public:
 	dielectric(float ref_idx_in) : ref_idx(ref_idx_in) {}
 	virtual bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered) const {
 		vec3 outward_normal;
+		vec3 reflected = reflect(r_in.direction(), rec.normal);
 		float ni_over_nt;
 		attenuation = vec3(1.0, 1.0, 1.0);
 		vec3 refracted;
+		float reflect_prob;
+		float cosine;
 		if (dot(r_in.direction(), rec.normal) > 0) {
 			outward_normal = -rec.normal;
 			ni_over_nt = ref_idx;
+			cosine = ref_idx * dot(r_in.direction(), rec.normal) / r_in.direction().length();
 		}
 		else {
 			outward_normal = rec.normal;
 			ni_over_nt = (1.0 / ref_idx);
+			cosine = -dot(r_in.direction(), rec.normal) / r_in.direction().length();
 		}
 
 		if (refract_author(r_in.direction(), outward_normal, ni_over_nt, refracted)) {
-			scattered = ray(rec.p, refracted);
-			return true;
+			reflect_prob = schlick(cosine, ref_idx);
 		}
 		else {
-			vec3 reflected = reflect(r_in.direction(), rec.normal);
 			scattered = ray(rec.p, reflected);
 			return true;
+			//reflect_prob = 1.0f;
 		}
+
+		if ((float(rand()) / float(RAND_MAX)) < reflect_prob) {
+			scattered = ray(rec.p, reflected);
+		}
+		else {
+			scattered = ray(rec.p, refracted);
+		}
+
+		return true;
 	}
 };
--- Ray_Tracer/Ray_Tracer/src/main.cpp
+++ Ray_Tracer/Ray_Tracer/src/main.cpp
@@ -82,7 +82,7 @@ int main() {
 	float fHeight = float(height);
 
 	camera cam;
-	int sample_count = 64;
+	int sample_count = 100;
 	srand((unsigned int)time(NULL));
 
 	int ir, ig, ib;
@@ -99,13 +99,14 @@ int main() {
 	float xPercent;
 	float yPercent;
 
-	hitable *list[4];
-	list[0] = new sphere( vec3(0, 0.0f, -1), 0.5f, new lambertian(vec3(0.8f, 0.3f, 0.03f)) );
+	hitable *list[5];
+	list[0] = new sphere( vec3(0, 0.0f, -1), 0.5f, new lambertian(vec3(0.1f, 0.2f, 0.5f)) );
 	list[1] = new sphere( vec3(0, -100.5f, -1), 100.0f, new lambertian(vec3(0.8f, 0.8f, 0.0f)) );
 
-	list[2] = new sphere(vec3(1, 0, -1), 0.5f, new metal(vec3(0.8f, 0.6f, 0.2f), 1.0f));
+	list[2] = new sphere(vec3(1, 0, -1), 0.5f, new metal(vec3(0.8f, 0.6f, 0.2f)));
 	list[3] = new sphere(vec3(-1, 0, -1), 0.5f, new dielectric(1.5));
-	hitable* world = new hitable_list(list, 4);
+	list[4] = new sphere(vec3(-1, 0, -1), -0.45f, new dielectric(1.5));
+	hitable* world = new hitable_list(list, 5);
 
 	for (int i = height - 1; i >= 0; i--) {
 		for (int j = 0; j < width; j++) {

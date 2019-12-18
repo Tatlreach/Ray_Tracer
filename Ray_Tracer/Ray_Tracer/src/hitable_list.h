0944a9d874337d94251e6177b83f2704fb50ca5e
Mike <Tatlreach@gmail.com>
2019-12-17 19:07:44 -0600

    hitable_list code reformated.

--- Ray_Tracer/Ray_Tracer/src/hitable_list.h
+++ Ray_Tracer/Ray_Tracer/src/hitable_list.h
@@ -6,26 +6,29 @@ public:
 	hitable **list;
 	int list_size;
 
-	hitable_list() {};
-	hitable_list(hitable** l, int l_size){
+	hitable_list() {}
+	hitable_list(hitable** l, int l_size) {
 		list = l;
 		list_size = l_size;
-	};
-	virtual bool hit(const ray& r, float dist_min, float dist_max, hit_record& rec) const;	///calls hit for all in the list
-	virtual bool hit_author(const ray& r, float dist_min, float dist_max, hit_record& rec) const;	///calls hit for all in the list
+	}
+
+	/// calls hit for all in the list
+	virtual bool hit(const ray& r, float dist_min, float dist_max, hit_record& rec) const;
+
+	/// calls hit for all in the list
+	virtual bool hit_author(const ray& r, float dist_min, float dist_max, hit_record& rec) const;
 };
 
-///author's version of iterating the hitable_list
+/// author's version of iterating the hitable_list
 bool hitable_list::hit(const ray& r, float dist_min, float dist_max, hit_record& rec) const {
 	bool has_hit = false;
 	hit_record temp_rec;
 
-	//loop through hitlist
-		//find the closest hit using generated record
+	// loop through hitlist
+		// find the closest hit using generated record
 	for (int i = 0; i < list_size; i++) {
 		if (list[i]->hit_author(r, dist_min, dist_max, temp_rec)) {
-
-			//list[i]->get_normal(r, temp_vec, temp_rec);
+			// list[i]->get_normal(r, temp_vec, temp_rec);
 			if (!has_hit || (temp_rec.dist < rec.dist)) {
 				rec = temp_rec;
 			}
@@ -38,7 +41,8 @@ bool hitable_list::hit(const ray& r, float dist_min, float dist_max, hit_record&
 bool hitable_list::hit_author(const ray& r, float dist_min, float dist_max, hit_record& rec) const {
 	return hit(r, dist_min, dist_max, rec);
 }
-///my version of iterating the hitable_list
+
+/// my version of iterating the hitable_list
 /*
 bool hitable_list::hit(const ray& r, float dist_min, float dist_max, hit_record& rec) {
 	bool has_hit = false;
@@ -47,9 +51,9 @@ bool hitable_list::hit(const ray& r, float dist_min, float dist_max, hit_record&
 	hit_record temp_rec;
 	vec3 nullVec(vec3(0, 0, 0));
 	vec3 temp_vec;
-	//loop through hitlist
-		//find the closest hit
-	//get normal vec of hit
+	// loop through hitlist
+		// find the closest hit
+	// get normal vec of hit
 
 	for (int i = 0; i < list_size; i++) {
 		if ( (temp_vec = list[i]->hit_occured(r)) != nullVec ) {

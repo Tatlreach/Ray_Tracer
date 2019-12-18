4b3a0eea8b1aebafdde4f9351a3a2155a24732be
Mike <Tatlreach@gmail.com>
2019-12-17 19:07:28 -0600

    hitable reformated. TODO added

--- Ray_Tracer/Ray_Tracer/src/hitable.h
+++ Ray_Tracer/Ray_Tracer/src/hitable.h
@@ -3,18 +3,21 @@
 #include "ray.h"
 class material;
 
+// TODO(Mike): consider moving hit_record to it's own .h
 struct hit_record {
 public:
-	float dist;	//direction distance  //dist from origin to sphere surface
-	vec3 p;		//coordinate of collision
+	float dist;		/// direction distance : dist from origin to sphere surface
+	vec3 p;			/// coordinate of collision
 	vec3 normal;
 	material *mat;
 };
 
 class hitable {
-public: 
+public:
 	virtual bool hit(const ray& r, float dist_min, float dist_max, hit_record& rec) const = 0;
 	virtual bool hit_author(const ray& r, float dist_min, float dist_max, hit_record& rec) const = 0;
-	//virtual vec3 hit_occured(const ray& r) const = 0;
-	//virtual vec3 get_normal(const ray& r, vec3 perpendic, hit_record& rec) const = 0;	
+
+	// depreciated functions
+	// virtual vec3 hit_occured(const ray& r) const = 0;
+	// virtual vec3 get_normal(const ray& r, vec3 perpendic, hit_record& rec) const = 0;
 };
